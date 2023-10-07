4a1258d79f206c307a15e4254e0d7abc6cdb8195
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2023-10-07 21:29:24 +0300

    Hot fix project

--- src/test/java/ru/netology/services/VacationPerYearTest.java
+++ src/test/java/ru/netology/services/VacationPerYearTest.java
@@ -3,11 +3,12 @@ package ru.netology.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationPerYearTest {
 
     @ParameterizedTest
-    @CsvFileSource(files="C:/Users/митя/IdeaProjects/Project12/src/test/recources/test.csv")
+    @CsvFileSource(resources = "test.csv")
 //    @CsvSource({
 //            "3,10_000,3_000,20_000",
 //            "2,100_000,60_000,150_000",
@@ -16,7 +17,7 @@ public class VacationPerYearTest {
     public void calculationOfVacationDaysWithCorrectData(int expected, int income, int expenses, int theshold) {
         VacationPerYear service = new VacationPerYear();
 
-        int actual = service.calculate(income, expenses, theshold);// доход от работы, месячные траты, порог для хождения в отпуск
+        int actual = service.calculate(income, expenses, theshold);
 
         Assertions.assertEquals(expected, actual);
 
@@ -27,7 +28,7 @@ public class VacationPerYearTest {
 //        VacationPerYear service = new VacationPerYear();
 //
 //        int expected = 3;
-//        int actual = service.calculate(10_000, 3_000, 20_000);// доход от работы, месячные траты, порог для хождения в отпуск
+//        int actual = service.calculate(10_000, 3_000, 20_000);
 //
 //        Assertions.assertEquals(expected, actual);
 //
